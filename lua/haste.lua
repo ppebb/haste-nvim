fd115045b81087b381ebf45793b445fe50e01bf8
ppeb <ppeb@ppeb.me>
2022-11-21 20:31:57 -0600

    fix nvim_echo must cannot be called in lua loop callback when using default notify provider

--- lua/haste.lua
+++ lua/haste.lua
@@ -15,6 +15,12 @@ function M.setup(conf)
     vim.api.nvim_create_user_command("Haste", function() M.upload() end, { nil })
 end
 
+local function notifywrap(message)
+    vim.schedule(function()
+        vim.notify(message)
+    end)
+end
+
 function M.upload()
     local stdout = uv.new_pipe()
     local stderr = uv.new_pipe()
@@ -40,31 +46,31 @@ function M.upload()
     )
 
     if not handle then
-        vim.notify(string.format(" Haste: Failed to spawn curl (%s)", pid))
+        notifywrap(string.format(" Haste: Failed to spawn curl (%s)", pid))
     end
 
     local function onstdout(err, data)
         if err then
-            vim.notify(string.format(" Haste: stdout - err: %s", err))
+            notifywrap(string.format(" Haste: stdout - err: %s", err))
         elseif data then
             local index = string.find(data, "key")
             local url = _config.url .. "/" .. string.sub(data, index + 6, -3)
             if index ~= nil then
-                vim.notify(string.format(" Haste: Your document is %s", url))
+                notifywrap(string.format(" Haste: Your document is %s", url))
                 if _config.setclip then
                     vim.schedule(function() vim.fn.setreg("+", url) end)
                 end
             -- else
-                -- vim.notify(string.format(" Haste: stdout - data: %s", data))
+                -- notifywrap(string.format(" Haste: stdout - data: %s", data))
             end
         end
     end
 
     local function onstderr(err, data)
         if err then
-            vim.notify(string.format(" Haste: stderr - err: %s", err))
+            notifywrap(string.format(" Haste: stderr - err: %s", err))
         -- elseif data then
-            -- vim.notify(string.format(" Haste: stderr - data: %s", data))
+            -- notifywrap(string.format(" Haste: stderr - data: %s", data))
         end
     end
 
